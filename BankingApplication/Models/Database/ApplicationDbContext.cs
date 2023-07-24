fa9c447f1153d933ebc5489fcc250be3ef75492b
Gaurav Mori <gkmori444@gmail.com>
2023-07-24 19:27:48 +0530

    Created All The Entities

---
+++ BankingApplication/Models/Database/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankingApplication.Models.Database
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option):base(option)
+        {
+                
+        }
+        public DbSet<AccountType> AccountTypes { get; set; }
+        public DbSet<BankAccount> BankAccount { get; set; }
+        public DbSet<BankAccountPosting> BankAccountPosting { get; set; }
+        public DbSet<BankTransaction> BankTransaction { get; set; }
+        public DbSet<PaymentMethod> PaymentMethod { get; set; }
+        
+    }
+}
---
+++ BankingApplication/Models/PaymentMethod.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankingApplication.Models
+{
+    public class PaymentMethod
+    {
+        [Key]
+        [Required]
+        public int PaymentMethodId { get; set; }
+        [Required]
+        public string Name{ get; set; }
+    }
+}
